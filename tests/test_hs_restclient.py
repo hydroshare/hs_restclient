9e1c8fecfbb2a36c21d589ad8abfdd415dfea17d
Brian Miles <selimnairb@gmail.com>
2015-05-21 17:07:57 -0400

    Add mocked test for getResourceList without filtering

--- tests/test_hs_restclient.py
+++ tests/test_hs_restclient.py
@@ -37,5 +37,27 @@ class TestGetResourceTypes(unittest.TestCase):
         self.assertSetEqual(res_type_proto, res_types)
 
 
+class TestGetResourceList(unittest.TestCase):
+
+    def setUp(self):
+        self.resource_titles = ["Untitled resource 1", "Untitled resource 2",
+                                "Create via REST API and Python client library",
+                                "Create via REST API and Python client library 2",
+                                "Create via REST API and Python client library 3"]
+        self.resource_ids = ['c165b6f8ec64405da004bfb8890c35ae',
+                             '1a63d6c24b9b4b42b9a97abfab94f9c6',
+                             '5a6e5a992b1046ae820fd3e79ee36107',
+                             'c817e936ac1147639787c0d4688d6319',
+                             '408acb6b870d4297b8b036edcd9c3c58']
+
+    @with_httmock(mocks.hydroshare.resourceList_get)
+    def test_get_resource_list(self):
+        hs = HydroShare()
+        res_list = hs.getResourceList()
+
+        for (i, r) in enumerate(res_list):
+            self.assertEquals(r['resource_title'], self.resource_titles[i])
+            self.assertEquals(r['resource_id'], self.resource_ids[i])
+
 if __name__ == '__main__':
     unittest.main()
