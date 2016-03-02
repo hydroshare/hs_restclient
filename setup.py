e450a2e3624dfd0d49ac818d4017392c0a70877c
Brian Miles <selimnairb@gmail.com>
2016-03-01 22:47:48 -0600

    1.2.2.dev2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.1.post1',
+    version='1.2.2.post2',
 
     description='HydroShare REST API client library',
     long_description=long_description,
